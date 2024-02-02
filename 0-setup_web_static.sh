6971979cabbfc0c2f305389807de6c96636d4bf4
Joe Egboka <joeegboka@gmail.com>
2024-02-02 02:50:28 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,13 +14,19 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
 # create fake content in index,html
-echo "Test Content" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # create symlink
 sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 
 # change ownership of /data/
-sudo chown -R ubuntu:ubuntu /path/
+sudo chown -R ubuntu:ubuntu /data/
 
 # configure server to listen on port 80
 sudo sed -i '/listen 80 default_server;/a location \/hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
