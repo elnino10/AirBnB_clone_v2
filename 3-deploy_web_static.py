c0c9eee5fa658f59a7ff65a0418d41c6e724362b
Joe Egboka <joeegboka@gmail.com>
2024-02-05 01:50:30 +0100

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Write a Fabric script (based on the file 2-do_deploy_web_static.py) that
-creates and distributes an archive to your web servers, using the function deploy
+creates and distributes an archive to your web servers, using the function
+deploy
 """
 
 from datetime import datetime
