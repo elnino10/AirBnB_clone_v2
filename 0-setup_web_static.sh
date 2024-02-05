b2709fb682e0bca8bb3affe4d435e804921f4de7
Joe Egboka <joeegboka@gmail.com>
2024-02-05 03:49:24 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -29,7 +29,32 @@ sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 sudo chown -R ubuntu:ubuntu /data/
 
 # configure server to listen on port 80
-sudo sed -i '/listen 80 default_server;/a location \/hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+echo "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        server_name  _;
+
+        location  /  {
+                root     /var/www/html;
+                index    index.html;
+                add_header X-Served-By $HOSTNAME;
+        }
+
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+
+        location  /redirect_me  {
+                return 301 https://joe-chidiebere-portfolio.onrender.com/;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html  {
+                root /var/www/html;
+                internal;
+                return 404 'Ceci n\'est pas une page\n';
+        }
+}"  |  sudo tee /etc/nginx/sites-available/default
 
 # test server before restart
 sudo nginx -t
